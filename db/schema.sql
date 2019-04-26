a224d59251ae0833e4f1eab99754e5e1a02b051c
craigz123 <czimbler60@gmail.com>
2019-04-26 14:13:55 -0400

    re-added table layout to schema.db

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,11 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
+CREATE TABLE burgers
+(
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
+);

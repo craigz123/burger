88178df757f16dbcfc2f7a9a932aa8da2da2ca7d
craigz123 <czimbler60@gmail.com>
2019-04-26 14:05:03 -0400

    commented out the last change, heroku open giving an application error

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,5 @@
 DROP DATABASE IF EXISTS burgers_db;
--- Creates the "animals_db" database --
 CREATE DATABASE burgers_db;
 
--- Makes it so all of the following code will affect burgers_db --
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(255) NOT NULL,
-devoured BOOLEAN DEFAULT false,
-createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
-);
